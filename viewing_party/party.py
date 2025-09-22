2bc87c98a90d20e1cbbfa9531fb7afa7d99954c7
Veema Jhagru <veemajhagru@gmail.com>
2025-09-22 16:25:25 -0400

    implemented functions add_to_watched, add_to_watchlist, watch_movie

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,9 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    if title == None:
+
+    #Checks if any of the dictionary keys are None
+    if title == None or genre == None or rating == None:
         return None
 
     movie = {
@@ -11,6 +13,29 @@ def create_movie(title, genre, rating):
     }
 
     return movie
+
+def add_to_watched(data_dict, movie):
+    #Adds the movie to the list value of the key watched
+
+    data_dict["watched"].append(movie)
+
+    return data_dict
+
+def add_to_watchlist(data_dict, movie):
+
+    data_dict["watchlist"].append(movie)
+
+    return data_dict
+
+def watch_movie(data_dict, movie_title):
+    # Locate list index of movie in dictionary watchlist
+    #index = 0
+    for movie in data_dict["watchlist"]:
+        if movie_title in movie["title"]:
+            data_dict["watched"].append(movie)
+            data_dict["watchlist"].remove(movie)
+    print(data_dict)
+    return data_dict
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
